a9d675ddf7895d5bb48bd314e691384c015c45f8
duckmint10 <87639020+duckmint10@users.noreply.github.com>
2023-05-03 09:05:53 +0700

    ductm commit

---
+++ API/CinemaManagement/Controllers/WeuController/MovieInfoController.cs
@@ -0,0 +1,42 @@
+﻿using CinemaManagement.Controllers.CMSController;
+using CinemaManagement.Data;
+using CinemaManagement.DTOs.CmsDtos;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaManagement.Controllers.WeuController
+{
+    public class MovieInfoController : BaseApiController
+    {
+        private readonly DataContext _context;
+        private readonly DapperContext _dapper;
+
+        public MovieInfoController(DataContext context, DapperContext dapper)
+        {
+            _context = context;
+            _dapper = dapper;
+        }
+        public async Task<List<MovieDto>> getListMovie()
+        {
+            var movies = _context.MstMovie.ToList().Where(e => e.IsDeleted == false);
+            var res = (from movie in movies
+                       select new MovieDto
+                       {
+                           Name = movie.Name,
+                           Image = movie.Image,
+                           Trailer = movie.Trailer,
+                           Director = movie.Director,
+                           Actor = movie.Actor,
+                           PublishDate = movie.PublishDate,
+                           Time = movie.Time,
+                           Languages = movie.Languages,
+                           Rated = movie.Rated,
+                           Description = movie.Description
+                       }).ToList();
+            return res;
+        }
+    }
+}
---
+++ API/CinemaManagement/DTOs/CmsDtos/CreateOrEditMovieDto.cs
@@ -0,0 +1,23 @@
+﻿using Abp.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaManagement.DTOs.CmsDtos
+{
+    public class CreateOrEditMovieDto : Entity<long?>
+    {
+        public string Name { get; set; }
+        public IFormFile? Image { get; set; }
+        public string Trailer { get; set; }
+        public string Director { get; set; }
+        public string Actor { get; set; }
+        public DateTime? PublishDate { get; set; }
+        public string Time { get; set; }
+        public string Languages { get; set; }
+        public string Rated { get; set; }
+        public string Description { get; set; }
+    }
+}
