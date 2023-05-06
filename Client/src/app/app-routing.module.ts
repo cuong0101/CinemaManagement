dcb048a6febf921a7862ed05901ed3a6bfb74b9f
duckmint10 <87639020+duckmint10@users.noreply.github.com>
2023-05-06 22:31:27 +0700

    ductm commit

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -6,7 +6,6 @@ import {SeatRankViewComponent } from './seat-rank/seat-rank-view/seat-rank-view.
 import { BodyComponent } from './navigation/body/body.component';
 import { MstUsersComponent } from './business/mst-users/mst-users.component';
 import { MstMovieComponent } from './business/mst-users/mst-movie/mst-movie.component';
-import { TestGridTableComponent } from './business/mst-users/test-grid-table/test-grid-table.component';
 const routes: Routes = [
   {path: "", redirectTo:"test", pathMatch:"full"},
   {path: "login", component:LoginComponent},
@@ -14,7 +13,6 @@ const routes: Routes = [
   {path: "user", component: MstUsersComponent},
   {path: "seatrank", component: MstSeatranksComponent},
   {path: "mstmovie", component: MstMovieComponent},
-  {path: "test", component: TestGridTableComponent}
 ];
 
 @NgModule({
