f8e0a254ad661f0bdd15623133aa95caf1def14f
duckmint10 <87639020+duckmint10@users.noreply.github.com>
2023-05-08 07:32:58 +0700

    ductm commit

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { MstSeatranksComponent } from './mst-seatranks/mst-seatranks.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import {SeatRankViewComponent } from './seat-rank/seat-rank-view/seat-rank-view.component';
 import { BodyComponent } from './navigation/body/body.component';
 import { MstUsersComponent } from './business/mst-users/mst-users.component';
 import { MstMovieComponent } from './business/mst-users/mst-movie/mst-movie.component';
