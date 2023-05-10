81f472bd6013c3a763a7be025c93c07adbcf2c5a
duckmint10 <87639020+duckmint10@users.noreply.github.com>
2023-05-10 22:25:08 +0700

    ductm commit

--- API/CinemaManagement/Controllers/CmsController/MovieController.cs
+++ API/CinemaManagement/Controllers/CmsController/MovieController.cs
@@ -37,22 +37,21 @@ namespace CinemaManagement.Controllers.CmsController
         public async Task<List<MovieDto>> GetAll()
         {
             var movies = _context.MstMovie.ToList().Where(e=>e.IsDeleted == false);
+
             var res = (from movie in movies
                        select new MovieDto
-                       { 
-                        Id = movie.Id,
-                        Name = movie.Name,
-                        Image = movie.Image,
-                        Trailer = movie.Trailer,
-                        Director = movie.Director,
-                        Actor = movie.Actor,
-                        PublishDate = movie.PublishDate.Value.Date,
-                        /*Time = (movie.Time.Value.Hours < 10 ? "0"+movie.Time.Value.Hours : 
-                        movie.Time.Value.Hours+"")+":"+(movie.Time.Value.Minutes < 10 ? 
-                        "0"+movie.Time.Value.Minutes : movie.Time.Value.Minutes+""),*/
-                        Languages = movie.Languages,
-                        Rated = movie.Rated,
-                        Description = movie.Description
+                       {
+                           Id = movie.Id,
+                           Name = movie.Name,
+                           Image = movie.Image,
+                           Trailer = movie.Trailer,
+                           Director = movie.Director,
+                           Actor = movie.Actor,
+                           PublishDate = movie.PublishDate.Value.Date,
+                           Time = movie.Time.ToString(),
+                           Languages = movie.Languages,
+                           Rated = movie.Rated,
+                           Description = movie.Description
                        }).ToList();
             return res;
         }
