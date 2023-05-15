eb7e06f19e726fda7e917b68b7984eded7766bef
duckmint10 <87639020+duckmint10@users.noreply.github.com>
2023-05-15 22:28:38 +0700

    ductm commit

---
+++ API/CinemaManagement/Controllers/WeuController/ShowtimeInfoController.cs
@@ -0,0 +1,81 @@
+﻿using AutoMapper;
+using CinemaManagement.Controllers.CmsController;
+using CinemaManagement.Data;
+using CinemaManagement.DTOs.CmsDtos.ShowTime;
+using CinemaManagement.DTOs.WeuDtos;
+using CinemaManagement.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaManagement.Controllers.WeuController
+{
+    public class ShowtimeInfoController : BaseApiController_new
+    {
+        private readonly DataContext _context;
+        private readonly DapperContext _dapper;
+
+        public ShowtimeInfoController(DataContext context, DapperContext dapper, IMapper mapper) : base(mapper)
+        {
+            _context = context;
+            _dapper = dapper;
+        }
+
+        [HttpGet("getListShowTime")]
+        public async Task<List<ShowTimeDto>> getListShowTime(DateTime? date, string MovieName)
+        {
+            var res = await (from sh in _context.MstShowTimes.AsNoTracking()
+                      .Where(e => e.IsDeleted == false)
+                      .Where(e => !date.HasValue || date.Value.Date == e.StartTime.Date)
+                             join m in _context.MstMovie.AsNoTracking()
+                             .Where(e => e.IsDeleted == false)
+                             .Where(e => string.IsNullOrEmpty(MovieName) || e.Name.Contains(MovieName))
+                             on sh.MovieId equals m.Id
+                             join r in _context.MstRooms.AsNoTracking()
+                            .Where(e => e.IsDeleted == false)
+                             on sh.RoomId equals r.Id
+                             select new ShowTimeDto
+                             {
+                                 Id = sh.Id,
+                                 StartDate = sh.StartTime.Date,
+                                 StartTime = sh.StartTime.ToString("HH:mm"),
+                                 MovieName = m.Name,
+                                 RoomName = r.Name
+                             }).ToListAsync();
+
+            return res;
+        }
+
+        [HttpGet("getListTicketByShowTime")]
+
+        public async Task<List<TicketDto>> getListSeatByShowTime(long IdShowTime)
+        {
+            var ticket = (from tick in _context.MstTicket
+                          .Where(e=>e.IsDeleted == false)
+                          join show in _context.MstShowTimes
+                          .Where(e=>e.IsDeleted == false)
+                          on tick.ShowTimeId equals show.Id
+                          join seat in _context.MstSeats 
+                          .Where(e=>e.IsDeleted == false)
+                          on tick.SeatId equals seat.Id
+                          join sr in _context.MstSeatRank
+                          .Where(e=>e.IsDeleted == false)
+                          on seat.IdSeatRank equals sr.Id
+
+                          select new TicketDto
+                          {
+                              Id = tick.Id,
+                              Location = seat.Row + seat.Column,
+                              Status = tick.Status,
+                              Price = tick.Price,
+                              CustomerId = tick.CustomerId,
+                              EmpoyeeId = tick.EmployeeId,
+                              SeatRankName = sr.Name
+                          }).ToList();
+            return ticket;
+        }
+    }
+}
---
+++ API/CinemaManagement/DTOs/WeuDtos/TicketDto.cs
@@ -0,0 +1,13 @@
+﻿namespace CinemaManagement.DTOs.WeuDtos
+{
+    public class TicketDto
+    {
+        public long Id { get; set; }
+        public string Location { get; set; }
+        public int Status { get; set; }
+        public decimal Price { get; set; }
+        public string SeatRankName { get; set; }
+        public long? CustomerId { get; set; }
+        public long? EmpoyeeId { get; set; }
+    }
+}
