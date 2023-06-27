6ce73559525cd749f0759e42d9718efdb0a2d712
ThinhNguyen9999 <thinhvidia2610@gmail.com>
2023-06-27 22:41:24 +0700

    nhat true false

--- API/CinemaManagement/Entities/PolicyGifts.cs
+++ API/CinemaManagement/Entities/PolicyGifts.cs
@@ -6,10 +6,7 @@ namespace CinemaManagement.Entities
 {
     public class PolicyGifts : FullAuditedEntity<long>, IEntity<long>
     {
-<<<<<<< HEAD
         public long? Id { get; set; }
-=======
->>>>>>> 393aadd1f38e946af84b18f46ad381c1b05e9f55
         public string GiftName { get; set; }
         public int Point { get; set; } // Điểm quy đổi
         public bool IsStatus { get; set; } // trạng thái hoạt động
--- Client/src/app/_interfaces/policygift.ts
+++ Client/src/app/_interfaces/policygift.ts
@@ -2,7 +2,7 @@ export class  PolicyGift{
     id?: number;
     giftName?: string;
     point?: number;
-    isStatus?: number;
+    isStatus?: boolean;
     fromDate?: Date;
     toDate?: Date;
     description?: string;
