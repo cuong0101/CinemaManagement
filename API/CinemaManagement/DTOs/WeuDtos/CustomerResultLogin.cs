c72ade70b28fda6370728009ca316dc069c1a70c
cuong0101 <phuongcuong1504@gmail.com>
2023-06-28 23:00:52 +0700

    modify login customer

---
+++ API/CinemaManagement/DTOs/WeuDtos/CustomerResultLogin.cs
@@ -0,0 +1,16 @@
+﻿namespace CinemaManagement.DTOs.WeuDtos
+{
+    public class CustomerResultLogin
+    {
+        public CustomerResultLogin(string username, string accessToken, string refreshToken)
+        {
+            this.username = username;
+            this.accessToken=accessToken;
+            this.refreshToken=refreshToken;
+        }
+
+        public string username { get; set; }
+        public string accessToken { get; set; }
+        public string refreshToken { get; set; }
+    }
+}
